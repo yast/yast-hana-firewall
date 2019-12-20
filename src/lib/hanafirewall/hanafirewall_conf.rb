f7b207f0e8216593bacf37b2f54cdb529d7eea1a
Josef Reidinger <jreidinger@suse.cz>
2019-12-20 15:34:53 +0100

    make rubocop happy and fix code to avoid unknown method textdomain

--- src/lib/hanafirewall/hanafirewall_conf.rb
+++ src/lib/hanafirewall/hanafirewall_conf.rb
@@ -1,4 +1,3 @@
-# encoding: utf-8
 # ------------------------------------------------------------------------------
 # Copyright (c) 2016 SUSE LINUX GmbH, Nuernberg, Germany.
 #
@@ -17,62 +16,63 @@
 # Author: Howard Guo <hguo@suse.com>
 # Author: Peter Varkoly <varkoly@suse.com>
 
-require 'yast'
-require 'open3'
-require 'pathname'
-require 'hanafirewall/sysconfig_editor'
-require 'y2firewall/firewalld'
+require "yast"
+require "open3"
+require "pathname"
+require "hanafirewall/sysconfig_editor"
+require "y2firewall/firewalld"
 
-Yast.import 'Service'
+Yast.import "Service"
 
 module HANAFirewall
-    # get_hana_service_names returns all service short names.
-    def get_service_names
-          Dir.glob('/etc/hana-firewall/*').map{ |path|
-            file_name = Pathname.new(path).basename.to_s
+  # get_hana_service_names returns all service short names.
+  def get_service_names
+    Dir.glob("/etc/hana-firewall/*").map do |path|
+      file_name = Pathname.new(path).basename.to_s
 
-            # The firewall program calculates short name by keeping numbers and letters, write '-' for other characters, and convert the whole string into lower case.
-            short_name = ''
-            file_name.each_codepoint{ |c|
-              if /\p{L}/.match(c.chr)
-                short_name += c.chr
-              elsif /\p{N}/.match(c.chr)
-                short_name += c.chr
-              else
-                short_name += '-'
-              end
-            }
-            short_name.downcase
-          }
-    end
-
-    # get_inst_numbers retrieves the string array value from HANA_INSTANCE_NUMBERS configuration key.
-    def get_inst_numbers
-      HANAFirewall::SysconfigEditor.new(IO.read('/etc/sysconfig/hana-firewall')).get('HANA_INSTANCE_NUMBERS').split(/\s+/)
-    end
-
-    # set_inst_numbers writes down a new string array value into HANA_INSTANCE_NUMBERS configuration key.
-    def set_inst_numbers(new_array)
-      conf = HANAFirewall::SysconfigEditor.new(IO.read('/etc/sysconfig/hana-firewall'))
-      conf.set('HANA_INSTANCE_NUMBERS', new_array.sort.join(' '))
-      IO.write('/etc/sysconfig/hana-firewall', conf.to_text)
-    end
-
-    # get_zone_services returns all zone names and their corresponding service names.
-    def get_zone_services
-      ::Y2Firewall::Firewalld.instance.read
-      Hash[
-          ::Y2Firewall::Firewalld.instance.zones.map { |zone|
-            [zone.name, zone.services]
-          }
-      ]
-    end
-
-    # call the command line program to regenerate service definition files.
-    def regen_svcs
-      _, status = Open3.capture2e('hana-firewall', 'generate-firewalld-services')
-      if status.exitstatus != 0
-        raise "Command line error - " + outerr.gets
+      # The firewall program calculates short name by keeping numbers and letters,
+      # write '-' for other characters, and convert the whole string into lower case.
+      short_name = ""
+      file_name.each_codepoint do |c|
+        short_name += if /\p{L}/.match(c.chr)
+          c.chr
+        elsif /\p{N}/.match(c.chr)
+          c.chr
+        else
+          "-"
+        end
       end
+      short_name.downcase
     end
+  end
+
+  # get_inst_numbers retrieves the string array value from HANA_INSTANCE_NUMBERS configuration key.
+  def get_inst_numbers
+    HANAFirewall::SysconfigEditor.new(IO.read("/etc/sysconfig/hana-firewall"))
+      .get("HANA_INSTANCE_NUMBERS").split(/\s+/)
+  end
+
+  # set_inst_numbers writes down a new string array value into
+  # HANA_INSTANCE_NUMBERS configuration key.
+  def set_inst_numbers(new_array)
+    conf = HANAFirewall::SysconfigEditor.new(IO.read("/etc/sysconfig/hana-firewall"))
+    conf.set("HANA_INSTANCE_NUMBERS", new_array.sort.join(" "))
+    IO.write("/etc/sysconfig/hana-firewall", conf.to_text)
+  end
+
+  # get_zone_services returns all zone names and their corresponding service names.
+  def get_zone_services
+    ::Y2Firewall::Firewalld.instance.read
+    Hash[
+        ::Y2Firewall::Firewalld.instance.zones.map do |zone|
+          [zone.name, zone.services]
+        end
+    ]
+  end
+
+  # call the command line program to regenerate service definition files.
+  def regen_svcs
+    _, status = Open3.capture2e("hana-firewall", "generate-firewalld-services")
+    raise "Command line error - " + outerr.gets if status.exitstatus != 0
+  end
 end
--- src/lib/hanafirewall/sysconfig_editor.rb
+++ src/lib/hanafirewall/sysconfig_editor.rb
@@ -1,4 +1,3 @@
-# encoding: utf-8
 # ------------------------------------------------------------------------------
 # Copyright (c) 2016 SUSE LINUX GmbH, Nuernberg, Germany.
 #
@@ -17,193 +16,183 @@
 # Author: Howard Guo <hguo@suse.com>
 # Author: Peter Varkoly <varkoly@suse.com>
 
-require 'yast'
+require "yast"
 require "yast/i18n"
-require 'open3'
-require 'pathname'
+require "open3"
+require "pathname"
 
-Yast.import 'Service'
+Yast.import "Service"
 
 module HANAFirewall
-    # A smart sysconfig file editor that not only can handle key-value but also arrays.
-    # Array is identified by a key with _{number} suffix.
-    # Specially tailored for HANA firewall.
-    class SysconfigEditor
-        def initialize(text)
-            textdomain 'hanafirewall'
-            @lines = text.b.split("\n")
-        end
+  # A smart sysconfig file editor that not only can handle key-value but also arrays.
+  # Array is identified by a key with _{number} suffix.
+  # Specially tailored for HANA firewall.
+  class SysconfigEditor
+    include Yast::I18n
 
-        # Return all keys.
-        # If a key represents an array, then the key (rather than every index of it) will be returned.
-        def keys
-            ret = []
-            scan(/.*/) { | key, idx, val|
-                if idx == :nil || idx == 0
-                    ret << key
-                end
-                [:continue]
-            }
-            return ret
-        end
-
-        # Return value, or empty string if the key does not exist.
-        # Calling this function on an array key will return an empty string.
-        def get(key)
-            ret = ''
-            scan(/^#{key}$/) { |_, idx, val|
-                if idx == :nil
-                    ret = val
-                    [:stop]
-                else
-                    [:continue]
-                end
-            }
-            return ret
-        end
-
-        # Set value or create the key if it does not exist. Return true only if the key was found.
-        def set(key, val)
-            found = false
-            scan(/^#{key}$/) { |_, _, _|
-                found = true
-                [:set, val]
-            }
-            if !found
-                @lines << "#{key}=\"#{val}\""
-            end
-            return found
-        end
-
-        # Return length of array represented by the key.
-        # Return 0 if the key does not exist.
-        def array_len(key)
-            # By contract an array key must use an underscore before index number
-            max_idx = -1
-            scan(/^#{key}_*$/) { |_, idx, _|
-                if idx != nil && idx > max_idx
-                    max_idx = idx
-                end
-                [:continue]
-            }
-            return max_idx + 1
-        end
-
-        # Return array value that corresponds to the key and index.
-        # Return empty string if the key does not exist, or the index does not exist/out of bound.
-        def array_get(key, index)
-            ret = ''
-            scan(/^#{key}_*$/) { |_, idx, val|
-                if idx == index
-                    ret = val
-                    [:stop]
-                else
-                    [:continue]
-                end
-            }
-            return ret
-        end
-
-        # Set array value or create the key/index if it does not exist. Return true only if index was found.
-        def array_set(key, index, val)
-            found = false
-            scan(/^#{key}_*$/) { |_, idx, _|
-                if idx == index
-                    found = true
-                    [:set, val]
-                else
-                    [:continue]
-                end
-            }
-            if !found
-                @lines << "#{key}_#{index}=\"#{val}\""
-            end
-            return found
-        end
-
-        # Shrikn or enlarge the array to match the specified length.
-        # If specified length is 0, the entire array is erased.
-        def array_resize(key, new_len)
-            seen_idx = -1
-            scan(/^#{key}_*$/) { |_, idx, val|
-                if idx == nil
-                    [:continue]
-                elsif idx >= new_len
-                    if idx - 1 > seen_idx
-                        seen_idx = idx - 1
-                    end
-                    # Remove excesive indexes
-                    [:delete_continue]
-                else
-                    seen_idx = idx
-                end
-            }
-            # Introduce extra indexes
-            (seen_idx+1..new_len-1).each{ |idx|
-                @lines << "#{key}_#{idx}=\"\""
-            }
-        end
-
-        # Produce sysconfig file text, including all the modifications that have been done.
-        def to_text
-            return @lines.join("\n") + "\n"
-        end
-
-        # Scan all lines looking for keys matching the specified regex.
-        # Call code block with three parameters:
-        # - key without index number
-        # - index number, :nil if not an array
-        # - value
-        # Code block is expected to return an array:
-        # [:stop] - stop scanning and end
-        # [:continue] - continue scanning
-        # [:delete_stop] - delete the key (or array element) and stop
-        # [:delete_continue] - delete the key (or array element) and continue
-        # [:set, $new_value] - update the value (or array element value) and stop
-        def scan(key_regex, &block)
-            to_delete_idx = []
-            (0..@lines.length-1).each { |idx|
-                line = @lines[idx].strip
-                param = nil
-                # Test against array key
-                array_kiv = /^([A-Za-z0-9_]+)_([0-9])+="?([^"]*)"?$/.match(line)
-                if array_kiv != nil
-                    param = [array_kiv[1], array_kiv[2].to_i, array_kiv[3]]
-                else
-                    # Test against ordinary key
-                    kv = /^([A-Za-z0-9_]+)="?([^"]*)"?$/.match(line)
-                    if kv != nil
-                        param = [kv[1], :nil, kv[2]]
-                    end
-                end
-                if param != nil && key_regex.match(param[0])
-                    # Invoke code block and act upon the result
-                    result = block.call(*param)
-                    case result[0]
-                        when :stop
-                            break
-                        when :continue
-                            next
-                        when :delete_stop
-                            to_delete_idx << idx
-                            break
-                        when :delete_continue
-                            to_delete_idx << idx
-                            next
-                        when :set
-                            if array_kiv == nil
-                                @lines[idx] = "#{param[0]}=\"#{result[1]}\""
-                            else
-                                @lines[idx] = "#{param[0]}_#{param[1]}=\"#{result[1]}\""
-                            end
-                            break
-                    end
-                end
-            }
-            to_delete_idx.reverse.each {|idx|
-                @lines.slice!(idx)
-            }
-        end
-
+    def initialize(text)
+      textdomain "hanafirewall"
+      @lines = text.b.split("\n")
     end
+
+    # Return all keys.
+    # If a key represents an array, then the key (rather than every index of it) will be returned.
+    def keys
+      ret = []
+      scan(/.*/) do |key, idx, _val|
+        ret << key if [:nil, 0].include?(idx)
+        [:continue]
+      end
+      ret
+    end
+
+    # Return value, or empty string if the key does not exist.
+    # Calling this function on an array key will return an empty string.
+    def get(key)
+      ret = ""
+      scan(/^#{key}$/) do |_, idx, val|
+        if idx == :nil
+          ret = val
+          [:stop]
+        else
+          [:continue]
+        end
+      end
+      ret
+    end
+
+    # Set value or create the key if it does not exist. Return true only if the key was found.
+    def set(key, val)
+      found = false
+      scan(/^#{key}$/) do |_, _, _|
+        found = true
+        [:set, val]
+      end
+      @lines << "#{key}=\"#{val}\"" if !found
+      found
+    end
+
+    # Return length of array represented by the key.
+    # Return 0 if the key does not exist.
+    def array_len(key)
+      # By contract an array key must use an underscore before index number
+      max_idx = -1
+      scan(/^#{key}_*$/) do |_, idx, _|
+        max_idx = idx if !idx.nil? && idx > max_idx
+        [:continue]
+      end
+      max_idx + 1
+    end
+
+    # Return array value that corresponds to the key and index.
+    # Return empty string if the key does not exist, or the index does not exist/out of bound.
+    def array_get(key, index)
+      ret = ""
+      scan(/^#{key}_*$/) do |_, idx, val|
+        if idx == index
+          ret = val
+          [:stop]
+        else
+          [:continue]
+        end
+      end
+      ret
+    end
+
+    # Set array value or create the key/index if it does not exist.
+    # Return true only if index was found.
+    def array_set(key, index, val)
+      found = false
+      scan(/^#{key}_*$/) do |_, idx, _|
+        if idx == index
+          found = true
+          [:set, val]
+        else
+          [:continue]
+        end
+      end
+      @lines << "#{key}_#{index}=\"#{val}\"" if !found
+      found
+    end
+
+    # Shrikn or enlarge the array to match the specified length.
+    # If specified length is 0, the entire array is erased.
+    def array_resize(key, new_len)
+      seen_idx = -1
+      scan(/^#{key}_*$/) do |_, idx, _val|
+        if idx.nil?
+          [:continue]
+        elsif idx >= new_len
+          seen_idx = idx - 1 if idx - 1 > seen_idx
+          # Remove excesive indexes
+          [:delete_continue]
+        else
+          seen_idx = idx
+        end
+      end
+      # Introduce extra indexes
+      (seen_idx + 1..new_len - 1).each do |idx|
+        @lines << "#{key}_#{idx}=\"\""
+      end
+    end
+
+    # Produce sysconfig file text, including all the modifications that have been done.
+    def to_text
+      @lines.join("\n") + "\n"
+    end
+
+    # Scan all lines looking for keys matching the specified regex.
+    # Call code block with three parameters:
+    # - key without index number
+    # - index number, :nil if not an array
+    # - value
+    # Code block is expected to return an array:
+    # [:stop] - stop scanning and end
+    # [:continue] - continue scanning
+    # [:delete_stop] - delete the key (or array element) and stop
+    # [:delete_continue] - delete the key (or array element) and continue
+    # [:set, $new_value] - update the value (or array element value) and stop
+    def scan(key_regex, &block)
+      to_delete_idx = []
+      (0..@lines.length - 1).each do |idx|
+        line = @lines[idx].strip
+        param = nil
+        # Test against array key
+        array_kiv = /^([A-Za-z0-9_]+)_([0-9])+="?([^"]*)"?$/.match(line)
+        if !array_kiv.nil?
+          param = [array_kiv[1], array_kiv[2].to_i, array_kiv[3]]
+        else
+          # Test against ordinary key
+          kv = /^([A-Za-z0-9_]+)="?([^"]*)"?$/.match(line)
+          param = [kv[1], :nil, kv[2]] if !kv.nil?
+        end
+        next unless !param.nil? && key_regex.match(param[0])
+
+        # Invoke code block and act upon the result
+        result = block.call(*param)
+        case result[0]
+        when :stop
+          break
+        when :continue
+          next
+        when :delete_stop
+          to_delete_idx << idx
+          break
+        when :delete_continue
+          to_delete_idx << idx
+          next
+        when :set
+          @lines[idx] = if array_kiv.nil?
+            "#{param[0]}=\"#{result[1]}\""
+          else
+            "#{param[0]}_#{param[1]}=\"#{result[1]}\""
+          end
+          break
+        end
+      end
+      to_delete_idx.reverse.each do |idx|
+        @lines.slice!(idx)
+      end
+    end
+  end
 end
