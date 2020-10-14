5c774b69f7e0a122870c6512e5344c549e64df76
Peter Varkoly <varkoly@suse.com>
2020-10-14 16:00:30 +0200

    Fix undefined method calls in HANAFirewall::MainDialog

--- src/lib/hanafirewallui/main_dialog.rb
+++ src/lib/hanafirewallui/main_dialog.rb
@@ -47,6 +47,7 @@ module HANAFirewall
         include UIShortcuts
         include I18n
         include Logger
+	include HANAFirewall
 
         def initialize
             textdomain 'hanafirewall'
